d3fcb115c3ef12894df1d40cbadb30752068d8ad
Chao Hsu Lin <chaohsul0226@gmail.com>
2022-02-24 00:13:39 +0800

    [feat] support album

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -7,10 +7,14 @@
   const qsa = document.querySelectorAll.bind(document);
   const gen = document.createElement.bind(document);
 
+  let currentAlbum = null;
+  let albumIndex = 0;
+
   function init() {
     const modal = qs('.art-modal');
     requestImages();
-    modal.addEventListener('click', closeModal);
+    modal.addEventListener('mousemove', handleModalHover);
+    modal.addEventListener('click', handleModalClick);
   }
 
   function requestImages() {
@@ -42,6 +46,7 @@
     img.width = art.width;
     img.height = art.height;
     if (lazy) {
+      img.dataset.fullsrc = `images/art/${art.path}.png`;
       img.dataset.src = `images/downsized/${art.path}.webp`;
       img.classList.add('lazy');
     } else {
@@ -49,6 +54,7 @@
     }
     img.alt = art.name;
     img.addEventListener('click', () => openModal(art, img));
+    img.addEventListener('mouseover', useFullRes);
 
     const desc = gen('div');
     desc.classList.add('art__desc');
@@ -96,14 +102,30 @@
     });
   }
 
-  function openModal(art, galleryArt) {
+  function useFullRes() {
+    this.src = this.dataset.fullsrc;
+  }
 
+  function openModal(art, galleryArt) {
     galleryArt.src = `images/art/${art.path}.png`;
 
     const modal = qs('.art-modal');
+    modal.scrollTop = 0;
+    modal.scrollLeft = 0;
     const img = modal.querySelector('img');
-    img.src = `images/art/${art.path}.png`;
     img.alt = art.name;
+
+    if(art.album) {
+      albumIndex = 0;
+      currentAlbum = art.album;
+      qs('.album-counter').classList.remove('hidden');
+      img.src = `images/art/${art.album[albumIndex]}.png`;
+
+    } else {
+      currentAlbum = null;
+      qs('.album-counter').classList.add('hidden');
+      img.src = `images/art/${art.path}.png`;
+    }
 
     const modalDesc = qs('.art-modal__desc');
     const title = modalDesc.querySelector('.art__title');
@@ -116,9 +138,67 @@
     document.body.style.overflow = 'hidden';
   }
 
+  function prevAlbumImg() {
+    if(!currentAlbum) {
+      closeModal();
+      return;
+    }
+
+    albumIndex--;
+    if(albumIndex < 0) { albumIndex = currentAlbum.length - 1; }
+    id('albumCount').textContent = `${albumIndex + 1} / ${currentAlbum.length}`;
+    const img = qs('.art-modal img');
+    img.src = `images/art/${currentAlbum[albumIndex]}.png`;
+  }
+
+  function nextAlbumImg() {
+    if(!currentAlbum) {
+      closeModal();
+      return;
+    }
+
+    albumIndex++;
+    if(albumIndex > currentAlbum.length - 1) { albumIndex = 0; }
+    id('albumCount').textContent = `${albumIndex + 1} / ${currentAlbum.length}`;
+    const img = qs('.art-modal img');
+    img.src = `images/art/${currentAlbum[albumIndex]}.png`;
+  }
+
+  const ALBUM_HOVER_RATIO = 0.2;
+
+  function handleModalHover(e) {
+    if(currentAlbum === null) { return; }
+
+    const height = window.innerHeight * ALBUM_HOVER_RATIO;
+    if(e.clientY < height) {
+      id('prevBtn').classList.add('active');
+      id('nextBtn').classList.remove('active');
+    } else if (e.clientY < window.innerHeight - height) {
+      id('prevBtn').classList.remove('active');
+      id('nextBtn').classList.remove('active');
+    } else {
+      id('prevBtn').classList.remove('active');
+      id('nextBtn').classList.add('active');
+    }
+  }
+
+  function handleModalClick(e) {
+    if(currentAlbum === null) { return closeModal(); }
+
+    const height = window.innerHeight * ALBUM_HOVER_RATIO;
+    this.scrollTop = 0;
+    if(e.clientY < height) {
+      prevAlbumImg();
+    } else if (e.clientY < window.innerHeight - height) {
+      closeModal();
+    } else {
+      nextAlbumImg();
+    }
+  }
+
   function closeModal() {
-    this.classList.add('hidden');
     const modal = qs('.art-modal');
+    modal.classList.add('hidden');
     const img = modal.querySelector('img');
     img.src = '';
     img.alt = '';
